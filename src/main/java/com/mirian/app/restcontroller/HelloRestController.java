0c84d566e61d6edf64dbb5fbf64ea20c44fc7342
mirian228 <pariksini@mail.ru>
2023-05-28 15:40:13 +0400

    simplest get rest request

--- src/main/java/com/mirian/app/restcontroller/HelloRestController.java
+++ src/main/java/com/mirian/app/restcontroller/HelloRestController.java
@@ -1,7 +1,14 @@
 package com.mirian.app.restcontroller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/test")
 public class HelloRestController {
+    @GetMapping(path = "/hello")
+    public String helloWorld() {
+        return "Hello World!";
+    }
 }
