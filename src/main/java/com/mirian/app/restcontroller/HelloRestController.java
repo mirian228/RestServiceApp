625016b30f101c81830683de37b44d423b34ef48
mirian228 <pariksini@mail.ru>
2023-05-28 15:38:42 +0400

    Ignore modified

---
+++ src/main/java/com/mirian/app/restcontroller/HelloRestController.java
@@ -0,0 +1,7 @@
+package com.mirian.app.restcontroller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+}
---
+++ src/test/java/com/mirian/app/SpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirian.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
