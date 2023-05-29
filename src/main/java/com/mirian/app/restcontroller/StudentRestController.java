7510d7581273aba57de06be8c277da26d52bb73c
mirian228 <pariksini@mail.ru>
2023-05-29 15:04:30 +0400

    Implemented method for retrieving a single student

--- src/main/java/com/mirian/app/restcontroller/StudentRestController.java
+++ src/main/java/com/mirian/app/restcontroller/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.mirian.app.restcontroller;
 
 import com.mirian.app.model.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,16 +13,27 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> theStudents = new ArrayList<>();
+
+    private List<Student> theStudents;
+
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
         theStudents.add(new Student("Poornima", "Patel"));
         theStudents.add(new Student("Mario", "Rossi"));
         theStudents.add(new Student("Mary", "Smith"));
+    }
 
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return theStudents;
     }
 
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return theStudents.get(studentId);
+    }
 
 
 }
